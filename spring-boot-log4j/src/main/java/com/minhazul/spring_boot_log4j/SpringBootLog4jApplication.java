d6dba14b2150ef3b98a81b01416a05a8ec051f72
Minhazul Hayat Khan <minhaz1217@gmail.com>
2024-10-19 02:10:59 +0600

    added logging in spring boot gradle project

--- spring-boot-log4j/src/main/java/com/minhazul/spring_boot_log4j/SpringBootLog4jApplication.java
+++ spring-boot-log4j/src/main/java/com/minhazul/spring_boot_log4j/SpringBootLog4jApplication.java
@@ -1,13 +1,18 @@
 package com.minhazul.spring_boot_log4j;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootLog4jApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootLog4jApplication.class, args);
-	}
+    public static void main(String[] args) {
+        Logger logger = LogManager.getLogger(SpringApplication.class);
+        logger.info("Hello World!");
+
+        SpringApplication.run(SpringBootLog4jApplication.class, args);
+    }
 
 }
