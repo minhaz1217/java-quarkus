fc641f7148f57801b0370e22d7bfecef0bc76884
Minhazul Hayat Khan <minhaz1217@gmail.com>
2024-10-19 01:06:39 +0600

    java spring basic setup

---
+++ spring-boot-log4j/src/main/java/com/minhazul/spring_boot_log4j/SpringBootLog4jApplication.java
@@ -0,0 +1,13 @@
+package com.minhazul.spring_boot_log4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLog4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLog4jApplication.class, args);
+	}
+
+}
---
+++ spring-boot-log4j/src/test/java/com/minhazul/spring_boot_log4j/SpringBootLog4jApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minhazul.spring_boot_log4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLog4jApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
